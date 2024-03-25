4aefe5ca18815a607dbfe306ff3b13246391eddb
TomJcb11 <thomasjacob454@gmail.com>
2024-03-25 17:32:09 +0100

    add ssh and portainer on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+# Update packages
 sudo apt-get update
+
+# Install necessary packages for Docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker's stable repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update packages again
 sudo apt-get update
+
+# Install Docker
 sudo apt-get install docker-ce
+
+# Install OpenSSH Server
+sudo apt-get install openssh-server
+
+# Install Portainer
+docker volume create portainer_data
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
